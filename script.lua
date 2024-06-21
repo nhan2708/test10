6c95161463b8e56962a9199c46a10252860506d6
nhan2708 <163325777+nhan2708@users.noreply.github.com>
2024-06-21 17:05:43 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -2561,13 +2561,11 @@ local listfastattack = {'Normal','Slow','Super','Low'}
     DropdownDelayAttack:OnChanged(function(Value)
     _G.FastAttackZINER_Mode = Value
 	if _G.FastAttackWinterhub_Mode == "Slow" then
-		_G.Fast_Delay = 0.75
-	elseif _G.FastAttackWinterhub_Mode == "Normal" then
-		_G.Fast_Delay = 0.5
-	elseif _G.FastAttackWinterhub_Mode == "Super" then
 		_G.Fast_Delay = 0.25
-	elseif _G.FastAttackWinterhub_Mode == "Low" then
+	elseif _G.FastAttackWinterhub_Mode == "Normal" then
 		_G.Fast_Delay = 0.175
+	elseif _G.FastAttackWinterhub_Mode == "Super" then
+		_G.Fast_Delay = 0.15
 	 end
  end)
 
@@ -2768,64 +2766,7 @@ local listfastattack = {'Normal','Slow','Super','Low'}
             end
         end
         end)
-
-
---// auto farm chest
-
-if Third_Sea then
-local ToggleChest = Tabs.Main:AddToggle("ToggleChest", {
-    Title = "Auto Farm Chest Stop If Has Items Tp",
-    Description = "auto farm rương dạng Tp reset", 
-    Default = false })
-ToggleChest:OnChanged(function(Value)
-    _G.chestsea3 = Value
-end)
-Options.ToggleChest:SetValue(false)
-end
-
-if Second_Sea then
-local ToggleChest = Tabs.Main:AddToggle("ToggleChest", {
-    Title = "Auto Farm Chest Stop If Has Items Tp",
-    Description = "auto farm rương dạng Tp reset", 
-    Default = false })
-ToggleChest:OnChanged(function(Value)
-    _G.chestsea2 = Value
-end)
-Options.ToggleChest:SetValue(false)
-end
-
-local ToggleChestTween = Tabs.Main:AddToggle("ToggleChestTween", {
-    Title = "Auto Chest Tween",
-    Description = "farm chest dạng bay", 
-    Default = false })
-ToggleChestTween:OnChanged(function(Value)
-    AutoFarmChest = Value
-end)
-Options.ToggleChestTween:SetValue(false)
-_G.MagnitudeAdd = 0
-spawn(function()
-	while wait() do 
-		if AutoFarmChest then
-			for i,v in pairs(game:GetService("Workspace"):GetChildren()) do 
-				if v.Name:find("Chest") then
-					if game:GetService("Workspace"):FindFirstChild(v.Name) then
-						if (v.Position - game.Players.LocalPlayer.Character.HumanoidRootPart.Position).Magnitude <= 5000+_G.MagnitudeAdd then
-							repeat wait()
-								if game:GetService("Workspace"):FindFirstChild(v.Name) then
-									Tween(v.CFrame)
-								end
-							until AutoFarmChest == false or not v.Parent
-							Tween(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame)
-							_G.MagnitudeAdd = _G.MagnitudeAdd+1500
-							break
-						end
-					end
-				end
-			end
-		end
-	end
-end)
-
+	
       Tabs.Main:AddButton({
         Title = "Auto Redeem All Code",
         Description = "",
@@ -3091,7 +3032,7 @@ local StatusBone = Tabs.Main:AddParagraph({
 
 
 local ToggleBone = Tabs.Main:AddToggle("ToggleBone", {
-    Title = "Auto farm Bone (vip]",
+    Title = "Auto farm Bone",
     Description = "", 
     Default = false })
 ToggleBone:OnChanged(function(Value)
@@ -3176,7 +3117,7 @@ end
 if Third_Sea then
 local MiscFarm = Tabs.Main:AddSection("Katakuri Farm")
 local Mob_Kill_Cake_Prince = Tabs.Main:AddParagraph({
-    Title = "Check Katakuri Spam",
+    Title = "Check Katakuri Spawn",
     Content = ""
 })
 
@@ -3199,7 +3140,6 @@ end)
 
 local ToggleCake = Tabs.Main:AddToggle("ToggleCake", {
     Title = "Auto Farm Cake Prince",
-    Description = "(vip)", 
     Default = false })
 ToggleCake:OnChanged(function(Value)
  _G.CakePrince = Value
@@ -7553,31 +7493,6 @@ Tabs.Misc:AddButton({
 })
 
 
-local Mastery = Tabs.Misc:AddSection("Troll player")
-
-
-Tabs.Misc:AddButton({
-	Title = "Auto Rain Fruit",
-	Description = "",
-	Callback = function()
-        for i, v in pairs(game:GetObjects("rbxassetid://14759368201")[1]:GetChildren()) do
-            v.Parent = game.Workspace.Map
-            v:MoveTo(game.Players.LocalPlayer.Character.PrimaryPart.Position + Vector3.new(math.random(-50, 50), 100, math.random(-50, 50)))
-            if v.Fruit:FindFirstChild("AnimationController") then
-                v.Fruit:FindFirstChild("AnimationController"):LoadAnimation(v.Fruit:FindFirstChild("Idle")):Play()
-            end
-            v.Handle.Touched:Connect(function(otherPart)
-                if otherPart.Parent == game.Players.LocalPlayer.Character then
-                    v.Parent = game.Players.LocalPlayer.Backpack
-                    game.Players.LocalPlayer.Character.Humanoid:EquipTool(v)
-                end
-            end)
-        end
-	end
-})
-
-
-
 local Mastery = Tabs.Misc:AddSection("Misc")
 
 
@@ -7600,245 +7515,6 @@ spawn(function()
 	end)
 
 
-
-    local Mastery = Tabs.Misc:AddSection("Kaitun Cap")
-
-Tabs.Misc:AddButton({
-	Title = "Auto Show Items",
-	Description = "",
-	Callback = function()
-		do
-			local ui = game:GetService("CoreGui").RobloxGui.Modules.Profile:FindFirstChild("UILibrary")
-			if ui then ui:Destroy() end
-		end
-		local UserInputService = game:GetService("UserInputService")
-		local TweenService = game:GetService("TweenService")
-		local RunService = game:GetService("RunService")
-		local LocalPlayer = game:GetService("Players").LocalPlayer
-		local Mouse = LocalPlayer:GetMouse()
-	
-		do
-			local ui = game:GetService("Lighting"):FindFirstChild("Blur")
-			if ui then ui:Destroy() end
-		end
-	
-		local Blur = Instance.new("BlurEffect")
-	
-		TweenService:Create(
-			Blur,
-			TweenInfo.new(.4, Enum.EasingStyle.Back, Enum.EasingDirection.InOut),
-			{ Size = 50 }
-		):Play()
-		Blur.Parent = game.Lighting
-	
-		local UIStroke = Instance.new("UIStroke")
-		local UICorner = Instance.new("UICorner")
-	
-		local ScreenGui = Instance.new("ScreenGui")
-		local ImageButton = Instance.new("ImageButton")
-		local RobloxButton = Enum.ButtonStyle.RobloxButton
-	
-		ScreenGui.Parent = game.CoreGui
-		ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-	
-	
-		local cac = require(game:GetService("Players").LocalPlayer.PlayerGui.Main.UIController.Inventory)
-		local Inventory = game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("getInventory")
-		local Items = {}
-		local RaityLevel = { "Mythical", "Legendary", "Rare", "Uncommon", "Common" }
-		local RaityColor = {
-			["Common"] = Color3.fromRGB(179, 179, 179),
-			["Uncommon"] = Color3.fromRGB(92, 140, 211),
-			["Rare"] = Color3.fromRGB(140, 82, 255),
-			["Legendary"] = Color3.fromRGB(213, 43, 228),
-			["Mythical"] = Color3.fromRGB(238, 47, 50)
-		}
-		function GetRaity(color)
-			for k, v in pairs(RaityColor) do
-				if v == color then return k end
-			end
-		end
-	
-		for k, v in pairs(Inventory) do
-			Items[v.Name] = v
-		end
-	
-		local total = #getupvalue(cac.UpdateRender, 4)
-		local rac = {}
-		local allitem = {}
-		local total2 = 0
-		while total2 < total do
-			local i = 0
-			while i < 25000 and total2 < total do
-				game:GetService("Players").LocalPlayer.PlayerGui.Main.InventoryContainer.Right.Content.ScrollingFrame.CanvasPosition =
-					Vector2.new(0, i)
-				for k, v in pairs(game:GetService("Players").LocalPlayer.PlayerGui.Main.InventoryContainer.Right.Content.ScrollingFrame.Frame:GetChildren()) do
-					if v:IsA("Frame") and not rac[v.ItemName.Text] and v.ItemName.Visible == true then
-						local vaihuhu = GetRaity(v.Background.BackgroundColor3)
-						if vaihuhu then
-							if not allitem[vaihuhu] then
-								allitem[vaihuhu] = {}
-							end
-							table.insert(allitem[vaihuhu], v:Clone())
-						end
-						total2 = total2 + 1
-						rac[v.ItemName.Text] = true
-					end
-				end
-				i = i + 20
-			end
-			wait()
-		end
-		function GetXY(vec)
-			return vec * 100
-		end
-	
-		local tvk = Instance.new("UIListLayout")
-		tvk.FillDirection = Enum.FillDirection.Vertical
-		tvk.SortOrder = 2
-		tvk.Padding = UDim.new(0, 10)
-	
-		local Left = Instance.new("Frame", game.Players.LocalPlayer.PlayerGui.BubbleChat)
-		Left.BackgroundTransparency = 1
-		Left.Size = UDim2.new(.5, 0, 1, 0)
-		tvk.Parent = Left
-	
-		local Right = Instance.new("Frame", game.Players.LocalPlayer.PlayerGui.BubbleChat)
-		Right.BackgroundTransparency = 1
-		Right.Size = UDim2.new(.5, 0, 1, 0)
-		Right.Position = UDim2.new(.6, 0, 0, 0)
-		tvk:Clone().Parent = Right
-		for k, v in pairs(allitem) do
-			local cac = Instance.new("Frame", Left)
-			cac.BackgroundTransparency = 1
-			cac.Size = UDim2.new(1, 0, 0, 0)
-			cac.LayoutOrder = table.find(RaityLevel, k)
-	
-			local cac2 = Instance.new("Frame", Right)
-			cac2.BackgroundTransparency = 1
-			cac2.Size = UDim2.new(1, 0, 0, 0)
-			cac2.LayoutOrder = table.find(RaityLevel, k)
-	
-			local tvk = Instance.new("UIGridLayout", cac)
-			tvk.CellPadding = UDim2.new(.005, 0, .005, 0)
-			tvk.CellSize = UDim2.new(0, 70, 0, 70)
-			tvk.FillDirectionMaxCells = 100
-			tvk.FillDirection = Enum.FillDirection.Horizontal
-	
-			local ccc = tvk:Clone()
-			ccc.Parent = cac2
-			for k, v in pairs(v) do
-				if Items[v.ItemName.Text] and Items[v.ItemName.Text].Mastery then
-					if v.ItemLine2.Text ~= "Accessory" then
-						local bucac                  = v.ItemName:Clone()
-						bucac.BackgroundTransparency = 1
-						bucac.TextSize               = 10
-						bucac.TextXAlignment         = 2
-						bucac.TextYAlignment         = 2
-						bucac.ZIndex                 = 5
-						bucac.Text                   = Items[v.ItemName.Text].Mastery
-						bucac.Size                   = UDim2.new(.5, 0, .5, 0)
-						bucac.Position               = UDim2.new(.5, 0, .5, 0)
-						bucac.Parent                 = v
-					end
-					v.Parent = cac
-				elseif v.ItemLine2.Text == "Blox Fruit" then
-					v.Parent = cac2
-				end
-			end
-			cac.AutomaticSize = 2
-			cac2.AutomaticSize = 2
-		end
-		local ListHuhu = {
-			["Superhuman"] = Vector2.new(3, 2),
-			["DeathStep"] = Vector2.new(4, 3),
-			["ElectricClaw"] = Vector2.new(2, 0),
-			["SharkmanKarate"] = Vector2.new(0, 0),
-			["DragonTalon"] = Vector2.new(1, 5)
-		}
-		local MeleeG = Instance.new("Frame", Left)
-		MeleeG.BackgroundTransparency = 1
-		MeleeG.Size = UDim2.new(1, 0, 0, 0)
-		MeleeG.LayoutOrder = table.find(RaityLevel, k)
-		MeleeG.AutomaticSize = 2
-		MeleeG.LayoutOrder = 100
-		local tvk = Instance.new("UIGridLayout", MeleeG)
-		tvk.CellPadding = UDim2.new(.005, 0, .005, 0)
-		tvk.CellSize = UDim2.new(0, 70, 0, 70)
-		tvk.FillDirectionMaxCells = 100
-		tvk.FillDirection = Enum.FillDirection.Horizontal
-	
-		local cac = { "Superhuman", "ElectricClaw", "DragonTalon", "SharkmanKarate", "DeathStep", "GodHuman" }
-		for k, v in pairs(cac) do
-			if ListHuhu[v] and game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("Buy" .. v, true) == 1 then
-				local huhu = Instance.new("ImageLabel", MeleeG)
-				huhu.Image = "rbxassetid://9945562382"
-				huhu.ImageRectSize = Vector2.new(100, 100)
-				huhu.ImageRectOffset = ListHuhu[v] * 100
-			end
-		end
-		function formatNumber(v)
-			return tostring(v):reverse():gsub("%d%d%d", "%1,"):reverse():gsub("^,", "")
-		end
-	
-		game:GetService("Players").LocalPlayer.PlayerGui.Main.Beli.AnchorPoint = Vector2.new(0.5, 0.5)
-		game:GetService("Players").LocalPlayer.PlayerGui.Main.Beli.Position = UDim2.new(0, 1120, 0, 700)
-	
-		game:GetService("Players").LocalPlayer.PlayerGui.Main.Level.AnchorPoint = Vector2.new(0.5, 0.5)
-		game:GetService("Players").LocalPlayer.PlayerGui.Main.Level.Position = UDim2.new(0, 1150, 0, 750)
-		local Name = game:GetService("Players").LocalPlayer.PlayerGui.Main.Fragments:Clone()
-		Name.Name = "Name"
-		Name.Parent = game:GetService("Players").LocalPlayer.PlayerGui.Main.Beli
-		Name.Position = UDim2.new(0, 0, -1.5, 0)
-		Name.Size = UDim2.new(1, 0, 1, 0)
-		Name.TextColor3 = Color3.fromRGB(255, 255, 255)
-		Name.Text = game.Players.LocalPlayer.Name
-	
-		local Fragments = game:GetService("Players").LocalPlayer.PlayerGui.Main.Fragments:Clone()
-		Fragments.Name = "FragmentsCheck"
-		Fragments.Parent = game:GetService("Players").LocalPlayer.PlayerGui.Main.Beli
-		Fragments.Position = UDim2.new(0, 0, -0.75, 0)
-		Fragments.Size = UDim2.new(1, 0, 1, 0)
-		Fragments.Text = 'ƒ' .. formatNumber(game:GetService("Players").LocalPlayer.Data.Fragments.Value)
-		local args = {
-			[1] = "getAwakenedAbilities"
-		}
-	
-		game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer(unpack(args))
-		game.Players.LocalPlayer.PlayerGui.Main.AwakeningToggler.Visible = true
-		game:GetService("Players").LocalPlayer.PlayerGui.Main.AwakeningToggler.Position = UDim2.new(0.48, 10, 0.908, 2)
-		game:GetService("Players").LocalPlayer.PlayerGui.Main.AwakeningToggler.Size = UDim2.new(1, 0, 0.22, 0)
-	
-	
-		pcall(function()
-			game:GetService("Players").LocalPlayer.PlayerGui.Main.MenuButton.Visible = false
-		end)
-		pcall(function()
-			game:GetService("Players").LocalPlayer.PlayerGui.Main.RaceEnergy.Visible = false
-		end)
-		pcall(function()
-			game:GetService("Players").LocalPlayer.PlayerGui.Main.SafeZone.Visible = false
-		end)
-		pcall(function()
-			game:GetService("Players").LocalPlayer.PlayerGui.Main.HP.Visible = false
-		end)
-		pcall(function()
-			game:GetService("Players").LocalPlayer.PlayerGui.Backpack.Enabled.Visible = false
-		end)
-		pcall(function()
-			game:GetService("Players").LocalPlayer.PlayerGui.Main.Energy.Visible = false
-		end)
-		for k, v in pairs(game:GetService("Players").LocalPlayer.PlayerGui.Main:GetChildren()) do
-			if v:IsA("ImageButton") then
-				v:Destroy()
-			end
-		end
-		pcall(function()
-			game:GetService("Players").LocalPlayer.PlayerGui.Main.Compass.Visible = false
-		end)
-	end
-})
-
 local Mastery = Tabs.Misc:AddSection("Day")
 
 Tabs.Misc:AddButton({
@@ -7856,17 +7532,7 @@ function NoFog()
             v:Destroy()
         end
     end
-end
-
-Tabs.profile:AddParagraph({
-        Title = "Owner",
-        Content = "by Kai Wibu"
-    })
-  
-Tabs.profile:AddParagraph({
-        Title = "link Discord ZINER HUB",
-        Content = "https://discord.gg/Y8vrbPyPyR"
-    })      
+end   
 --------------------------------------------------------------------------------------------------------------------------------------------
 --Hop
 
