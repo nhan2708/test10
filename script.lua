472cf2c7540184a735da62d2b0b9ac0114545d85
nhan2708 <163325777+nhan2708@users.noreply.github.com>
2024-07-09 08:08:43 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -21,6 +21,7 @@ button.MouseButton1Click:Connect(onClick)
 ----------------------------------------------------------------------------------------------------------------------------------------------
 local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
 local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
+local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
 ----------------------------------------------------------------------------------------------------------------------------------------------
 local Window = Fluent:CreateWindow({
     Title = "NHN HUB",
